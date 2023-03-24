8518a774991db47cf1ed1c0a656b943cd3b1a16a
SofiaCurreri <sofiacurreri@gmail.com>
2023-03-24 09:00:33 +0100

    classe Prodotti + classe figlia Cibo

--- index.php
+++ index.php
@@ -1,1 +1,33 @@
-<?php ?>
+<?php 
+
+class Prodotti{
+    public $prezzo;
+    public $cani;
+    public $gatti;
+
+    public function __construct(
+        float $_prezzo,
+        bool $_cani,
+        bool $_gatti,
+    ) {
+        $this->prezzo = $_prezzo;
+        $this->cani = $_cani;
+        $this->gatti = $_gatti;
+    }
+}
+
+class Cibo extends Prodotti{
+    public $tipologia;
+
+    public function __construct(
+        float $_prezzo,
+        bool $_cani,
+        bool $_gatti,
+        string $_tipologia,
+    ){
+      parent::__construct($_prezzo, $_cani, $_gatti);  
+      $this->tipologia = $_tipologia;
+    }
+}
+
+?>
