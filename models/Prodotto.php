6f9aeb76c6b8d7556ef2b798ec996e4d2a9fb1f3
SofiaCurreri <sofiacurreri@gmail.com>
2023-03-25 17:40:42 +0100

    controlli su $prezzo e $categoria

--- models/Prodotto.php
+++ models/Prodotto.php
@@ -27,6 +27,28 @@ class Prodotto{
     public function getNome($nome) {
         return $this->$nome;
     }
+
+    public function setPrezzo($prezzo) {
+        if(!is_float($prezzo)) return false; //bloccata esecuzione se soddisfa almeno una delle condizioni
+        $this->prezzo = $prezzo;
+
+        return $this; //ritorna tutto l' oggetto
+    }
+
+    public function getPrezzo($prezzo) {
+        return $this->$prezzo;
+    }
+
+    public function setCategoria($categoria) {
+        if(!$categoria instanceof Categoria) return false; //se $categoria non è un' istanza di Categoria, blocco l' esecuzione
+        $this->categoria = $categoria;
+
+        return $this; //ritorna tutto l' oggetto
+    }
+
+    public function getCategoria($categoria) {
+        return $this->$categoria;
+    }
 }
 
 $prodotto = new Prodotto("Palla", 3.50, new Categoria("99246656"));
